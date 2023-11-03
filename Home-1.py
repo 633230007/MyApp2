9b3b51faa167df853cfb9cb9094f8ac051c01deb
633230007 <144574821+633230007@users.noreply.github.com>
2023-11-04 00:09:41 +0700

    Update Home-1.py

--- Home-1.py
+++ Home-1.py
@@ -1,84 +1,53 @@
-from sklearn.neighbors import KNeighborsClassifier
+import json
+import time
+import requests
+
 import streamlit as st
-import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
-import plotly.express as px
-import plotly.graph_objects as go
+from streamlit_lottie import st_lottie
+from streamlit_lottie import st_lottie_spinner
 
-st.header('kairung')
-#st.image("./pic/kairung.jpg")
-col1, col2, col3 = st.columns(3)
+st.set_page_config(
+    page_title="Theerakan_633230007 24.2",
+    page_icon= ":bar_chart:",
+)
 
-with col1:
-   st.header("Versicolor")
-   st.image("./pic/iris1.jpg")
+st.image('./images/header.jpg')
+st.sidebar.markdown("# Home #")
 
-with col2:
-   st.header("Verginiga")
-   st.image("./pic/iris2.jpg")
+with open('./files/wave.css') as f:
+    css = f.read()
+st.markdown(f'<style>{css}</style>', unsafe_allow_html=True)
 
-with col3:
-   st.header("Setosa")
-   st.image("./pic/iris3.jpg")
+st.write("## ความเครียด")
+st.write("## จัดทำโดย")
+st.write("## ธีรกานต์  คุ้มชุมแสง ")
 
-html_7 = """
-<div style="background-color:#EC7063;padding:15px;border-radius:15px 15px 15px 15px;border-style:'solid';border-color:black">
-<center><h5>สถิติข้อมูลดอกไม้</h5></center>
-</div>
-"""
-st.markdown(html_7, unsafe_allow_html=True)
-st.markdown("")
 
-dt = pd.read_csv("./data/iris.csv")
-st.write(dt.head(10))
+st.image('./images/diabetes.jpg')
 
-dt1 = dt['petal.length'].sum()
-dt2 = dt['petal.width'].sum()
-dt3 = dt['sepal.length'].sum()
-dt4 = dt['sepal.width'].sum()
+import json
+import time
+import requests
 
-dx = [dt1, dt2, dt3, dt4]
-dx2 = pd.DataFrame(dx, index=["d1", "d2", "d3", "d4"])
+import streamlit as st
+from streamlit_lottie import st_lottie
+from streamlit_lottie import st_lottie_spinner
 
-if st.button("แสดงการจินตทัศน์ข้อมูล"):
-   #st.write(dt.head(10))
-   st.bar_chart(dx2)
-   st.button("ไม่แสดงข้อมูล")
-else:
-    st.write("ไม่แสดงข้อมูล")
+def load_lottieurl(url: str):
+    r = requests.get(url)
+    if r.status_code != 200:
+        return None
+    return r.json()
 
-html_8 = """
-<div style="background-color:#6BD5DA;padding:15px;border-radius:15px 15px 15px 15px;border-style:'solid';border-color:black">
-<center><h5>ทำนายข้อมูล</h5></center>
-</div>
-"""
-st.markdown(html_8, unsafe_allow_html=True)
-st.markdown("")
+lottie_url_hello = "https://lottie.host/3f647b41-61bf-4d39-93c3-0433420604cc/8NtmEbAWmO.json"
+lottie_url_download = "https://lottie.host/290016a6-d650-4a32-85c7-4d77b0a892ca/4rNL8XZeZt.json"
+lottie_hello = load_lottieurl(lottie_url_hello)
+lottie_download = load_lottieurl(lottie_url_download)
 
-pt_len = st.slider("กรุณาเลือกข้อมูล petal.length")
-pt_wd = st.slider("กรุณาเลือกข้อมูล petal.width")
 
-sp_len = st.number_input("กรุณาเลือกข้อมูล sepal.length")
-sp_wd = st.number_input("กรุณาเลือกข้อมูล sepal.width")
+st_lottie(lottie_hello, key="hello")
 
-if st.button("ทำนายผล"):
-    #st.write("ทำนาย")
-   dt = pd.read_csv("./data/iris.csv") 
-   X = dt.drop('variety', axis=1)
-   y = dt.variety   
-   Knn_model = KNeighborsClassifier(n_neighbors=3)
-   Knn_model.fit(X, y)   
-   x_input = np.array([[pt_len, pt_wd, sp_len, sp_wd]])
-   st.write(Knn_model.predict(x_input))
-   
-   out=Knn_model.predict(x_input)
-
-   if out[0] == 'Setosa':
-    st.image("./pic/iris1.jpg")
-   elif out[0] == 'Versicolor':       
-    st.image("./pic/iris2.jpg")
-   else:
-    st.image("./pic/iris3.jpg")
-else:
-    st.write("ไม่ทำนาย")
+if st.button("Download"):
+    with st_lottie_spinner(lottie_download, key="download"):
+        time.sleep(5)
+    st.balloons()
